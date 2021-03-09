224d0a8895594135b36986d6b749368b1eb55936
Cedric <cedric.j.rodriguez@protonmail.com>
2021-03-09 17:22:51 +0100

    Changed the legacy vtk class QVTKOpenGLWidget to QVTKOpenGLNativeWidget

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,56 +1,52 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
-#include <vtkPSphereSource.h>
 #include <vtkActor.h>
+#include <vtkPSphereSource.h>
 #include <vtkPolyDataMapper.h>
 
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent), ui(new Ui::MainWindow),
+      mRenderWindow(vtkSmartPointer<vtkGenericOpenGLRenderWindow>::New()),
+      mRenderer(vtkSmartPointer<vtkRenderer>::New()),
+      mInteractor(vtkSmartPointer<QVTKInteractor>::New()),
+      mInteractorStyle(vtkSmartPointer<vtkInteractorStyle>::New()) {
+  ui->setupUi(this);
 
-MainWindow::MainWindow(QWidget *parent):
-    QMainWindow(parent),
-    ui(new Ui::MainWindow),
-    mRenderWindow(vtkSmartPointer<vtkGenericOpenGLRenderWindow>::New()),
-    mRenderer(vtkSmartPointer<vtkRenderer>::New()),
-    mInteractor(vtkSmartPointer<QVTKInteractor>::New()),
-    mInteractorStyle(vtkSmartPointer<vtkInteractorStyle>::New())
-{
-    ui->setupUi(this);
+  // Setup rendering
+  mRenderWindow->AddRenderer(mRenderer);
+  mRenderWindow->SetInteractor(mInteractor);
+  ui->openGLNativeWidget->SetRenderWindow(mRenderWindow);
+  mInteractor->SetInteractorStyle(mInteractorStyle);
+  mInteractor->Initialize();
 
-    // Setup rendering
-    mRenderWindow->AddRenderer(mRenderer);
-    mRenderWindow->SetInteractor(mInteractor);
-    ui->openGLWidget->SetRenderWindow(mRenderWindow);
-    mInteractor->SetInteractorStyle(mInteractorStyle);
-    mInteractor->Initialize();
+  // Set background color
+  mRenderer->SetBackground(1, 0, 0);
 
-    // Set background color
-    mRenderer->SetBackground(1, 0, 0);
-
-    // Add the UI connections
-    QObject::connect(ui->pushButton, &QPushButton::clicked, this, &MainWindow::onDrawSphereClick);
+  // Add the UI connections
+  QObject::connect(ui->pushButton, &QPushButton::clicked, this,
+                   &MainWindow::onDrawSphereClick);
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
+MainWindow::~MainWindow() { delete ui; }
 
 void MainWindow::onDrawSphereClick() {
-    // Create sphere
-    vtkSmartPointer<vtkSphereSource> sphereSource = vtkSmartPointer<vtkPSphereSource>::New();
-    sphereSource->SetRadius(5);
-    sphereSource->Update();
+  // Create sphere
+  vtkSmartPointer<vtkSphereSource> sphereSource =
+      vtkSmartPointer<vtkPSphereSource>::New();
+  sphereSource->SetRadius(5);
+  sphereSource->Update();
 
-    // Create actor where the sphere id rendered
-    vtkSmartPointer<vtkPolyDataMapper> sphereMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
-    sphereMapper->SetInputData(sphereSource->GetOutput());
+  // Create actor where the sphere id rendered
+  vtkSmartPointer<vtkPolyDataMapper> sphereMapper =
+      vtkSmartPointer<vtkPolyDataMapper>::New();
+  sphereMapper->SetInputData(sphereSource->GetOutput());
 
-    vtkSmartPointer<vtkActor> sphere = vtkSmartPointer<vtkActor>::New();
-    sphere->SetMapper(sphereMapper);
+  vtkSmartPointer<vtkActor> sphere = vtkSmartPointer<vtkActor>::New();
+  sphere->SetMapper(sphereMapper);
 
-    // Add the sphere actor to the OpenGL
-    mRenderer->AddViewProp(sphere);
-    mRenderer->ResetCamera();
-    mRenderWindow->Render();
+  // Add the sphere actor to the OpenGL
+  mRenderer->AddViewProp(sphere);
+  mRenderer->ResetCamera();
+  mRenderWindow->Render();
 }
-
