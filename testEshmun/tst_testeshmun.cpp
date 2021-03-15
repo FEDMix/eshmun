6741f4220f83cd8503adf616a1aaa373ccc5e304
Yang <y.liu@esciencecenter.nl>
2021-03-15 11:03:11 +0100

    separate project and testing folders

---
+++ testEshmun/tst_testeshmun.cpp
@@ -0,0 +1,65 @@
+#include <QtTest/QtTest>
+#include <QCoreApplication>
+#include <QtWidgets>
+#include "mainwindow.h"
+
+// add necessary includes here
+
+class testEshmun : public QObject
+{
+    Q_OBJECT
+
+public:
+    testEshmun();
+    ~testEshmun();
+
+private slots:
+    void initTestCase();
+    // project test cases
+    // example test unit test
+    void strUpper();
+    // example ui test
+    void lineEdit();
+    void cleanupTestCase();
+    void test_case1();
+};
+
+testEshmun::testEshmun()
+{
+
+}
+
+testEshmun::~testEshmun()
+{
+
+}
+
+void testEshmun::initTestCase()
+{
+
+}
+
+void testEshmun::strUpper(){
+    QString strSample = "Eshmun";
+    QVERIFY(strSample.toUpper() == "ESHMUN");
+}
+
+void testEshmun::lineEdit(){
+    QLineEdit lineEdit;
+    QTest::keyClicks(&lineEdit, "button pressed");
+    QCOMPARE(lineEdit.text(), QString("button pressed"));
+}
+
+void testEshmun::cleanupTestCase()
+{
+
+}
+
+void testEshmun::test_case1()
+{
+
+}
+
+QTEST_MAIN(testEshmun)
+
+#include "tst_testeshmun.moc"
