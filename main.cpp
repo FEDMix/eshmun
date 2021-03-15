7e9972ec20f0dceaa1f1700f4c7c4df0fe963608
Yang <y.liu@esciencecenter.nl>
2021-03-15 10:51:25 +0100

    first testing case

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
-#include <QApplication>
-
 #include "mainwindow.h"
 
-int main(int argc, char *argv[]) {
-  QApplication a(argc, argv);
-  MainWindow w;
-  w.show();
-  return a.exec();
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,21 +2,28 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLabel>
+#include <QPixmap>
+#include <QPushButton>
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
+namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow {
-  Q_OBJECT
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
 
- public:
-  MainWindow(QWidget *parent = nullptr);
-  ~MainWindow();
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
 
- private:
-  Ui::MainWindow *ui;
+private slots:
+    void pushButton_selectSubject();
+    void pushButton_goBack();
+    void pushButton_subject1();
+
+private:
+    Ui::MainWindow *ui;
 };
-#endif  // MAINWINDOW_H
+#endif // MAINWINDOW_H
