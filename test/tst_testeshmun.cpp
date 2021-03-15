5ef7d9d60a94f9ebc2fb8e8560de9fc0d372ae38
Berend Weel <b.weel@esciencecenter.nl>
2021-03-15 16:10:44 +0100

    Updated build system to build both main program and tests

--- test/tst_testeshmun.cpp
+++ test/tst_testeshmun.cpp
@@ -1,64 +1,49 @@
-#include <QtTest/QtTest>
+#include "../src/mainwindow.h"
 #include <QCoreApplication>
+#include <QtTest/QtTest>
 #include <QtWidgets>
-#include "mainwindow.h"
+
 
 // add necessary includes here
 
-class testEshmun : public QObject
-{
-    Q_OBJECT
+class testEshmun : public QObject {
+  Q_OBJECT
 
 public:
-    testEshmun();
-    ~testEshmun();
+  testEshmun();
+  ~testEshmun();
 
 private slots:
-    void initTestCase();
-    // project test cases
-    // example test unit test
-    void strUpper();
-    // example ui test
-    void lineEdit();
-    void cleanupTestCase();
-    void test_case1();
+  void initTestCase();
+  // project test cases
+  // example test unit test
+  void strUpper();
+  // example ui test
+  void lineEdit();
+  void cleanupTestCase();
+  void test_case1();
 };
 
-testEshmun::testEshmun()
-{
+testEshmun::testEshmun() {}
 
+testEshmun::~testEshmun() {}
+
+void testEshmun::initTestCase() {}
+
+void testEshmun::strUpper() {
+  QString strSample = "Eshmun";
+  QVERIFY(strSample.toUpper() == "ESHMUN");
 }
 
-testEshmun::~testEshmun()
-{
-
+void testEshmun::lineEdit() {
+  QLineEdit lineEdit;
+  QTest::keyClicks(&lineEdit, "button pressed");
+  QCOMPARE(lineEdit.text(), QString("button pressed"));
 }
 
-void testEshmun::initTestCase()
-{
+void testEshmun::cleanupTestCase() {}
 
-}
-
-void testEshmun::strUpper(){
-    QString strSample = "Eshmun";
-    QVERIFY(strSample.toUpper() == "ESHMUN");
-}
-
-void testEshmun::lineEdit(){
-    QLineEdit lineEdit;
-    QTest::keyClicks(&lineEdit, "button pressed");
-    QCOMPARE(lineEdit.text(), QString("button pressed"));
-}
-
-void testEshmun::cleanupTestCase()
-{
-
-}
-
-void testEshmun::test_case1()
-{
-
-}
+void testEshmun::test_case1() {}
 
 QTEST_MAIN(testEshmun)
 
