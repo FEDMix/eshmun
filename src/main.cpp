a8d574284fbff52997ae30639af451b03efe2c8e
Pushpanjali Pawar <pushpanjalip55@gmail.com>
2021-03-18 16:49:24 +0100

    set tabscreen as main window

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
-
+#include "tabscreen.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.show();
+    tabscreen t;
+    t.show();
+//    w.show();
     return a.exec();
 }
