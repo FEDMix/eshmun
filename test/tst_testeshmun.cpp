310a1f471598cfd1c80db3a6b8c8d518b7d23589
Pushpanjali Pawar <pushpanjalip55@gmail.com>
2021-03-18 20:05:49 +0100

    Wrote tests to tab widget

--- test/tst_testeshmun.cpp
+++ test/tst_testeshmun.cpp
@@ -1,4 +1,5 @@
 #include "../src/mainwindow.h"
+#include "../src/tabwindow.h"
 #include <QCoreApplication>
 #include <QtTest/QtTest>
 #include <QtWidgets>
@@ -22,18 +23,23 @@ private slots:
   void lineEdit();
   void cleanupTestCase();
   void test_case1();
+//  void test_tabs();
 
   private:
       MainWindow main_window;
+      TabWindow tab_window;
+
 };
 
 testEshmun::testEshmun() {
-    main_window.show();
+//    main_window.show();
+    tab_window.show();
 }
 
 testEshmun::~testEshmun() {}
 
 void testEshmun::initTestCase() {}
+
 
 void testEshmun::strUpper() {
   QString strSample = "Eshmun";
@@ -49,14 +55,34 @@ void testEshmun::lineEdit() {
 void testEshmun::cleanupTestCase() {}
 
 void testEshmun::test_case1() {
-    QPushButton *ui_selectButton = main_window.findChild<QPushButton*>("buttonSelectSubjects");
-    QPushButton *ui_backButton = main_window.findChild<QPushButton*>("buttonGoBack");
+//    QPushButton *ui_selectButton = main_window.findChild<QPushButton*>("buttonSelectSubjects");
+//    QPushButton *ui_backButton = main_window.findChild<QPushButton*>("buttonGoBack");
 
-    QCOMPARE(ui_backButton->isVisible(),false);
-    QTest::mouseClick(ui_selectButton, Qt::LeftButton);
-    QCOMPARE(ui_backButton->isVisible(),true);
+//    QCOMPARE(ui_backButton->isVisible(),false);
+//    QTest::mouseClick(ui_selectButton, Qt::LeftButton);
+//    QCOMPARE(ui_backButton->isVisible(),true);
+    // get the main tab window
+      QTabWidget *ui_tab_widget = tab_window.findChild<QTabWidget*>("eshmun_tab_widget");
+      // check if number of tabs are 3
+      QCOMPARE(ui_tab_widget->count(),3);
+    // check if the current index is 0
+      QCOMPARE(ui_tab_widget->currentIndex(),1);
+    // check if the current tab is single view tab
+      QWidget *ui_single_view_tab = tab_window.findChild<QWidget*>("single_view_tab");
+      QCOMPARE(ui_tab_widget->currentWidget(),ui_single_view_tab);
 }
 
+//void testEshmun::test_tabs() {
+//    // get the main tab window
+//      QTabWidget *ui_tab_widget = tab_window.findChild<QTabWidget*>("eshmun_tab_widget");
+//      QCOMPARE(ui_tab_widget->count(),4);
+//    // check if the current index is 0
+//      QCOMPARE(ui_tab_widget->currentIndex(),1);
+//    // check if the current tab is single view tab
+//      QWidget *ui_single_view_tab = tab_window.findChild<QWidget*>("single_view_tab");
+//      QCOMPARE(ui_tab_widget->currentWidget(),ui_single_view_tab);
+
+//}
 QTEST_MAIN(testEshmun)
 
 #include "tst_testeshmun.moc"
