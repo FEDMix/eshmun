ef5320f0092f7e27b79df37c72797fd79f45a874
Maarten van Meersbergen <maartenvm@gmail.com>
2021-03-26 14:07:47 +0100

    added test changes

--- test/tst_eshmun.cpp
+++ test/tst_eshmun.cpp
@@ -3,16 +3,14 @@
 #include <QtTest/QtTest>
 #include <QtWidgets>
 
-<<<<<<< HEAD:test/tst_eshmun.cpp
-=======
-#include "../src/pages/patientView/annotation.h"
 #include "../src/components/VTKWidget/scenewidget.h"
-#include <vtkSmartPointer.h>
-#include <vtkImageData.h>
+#include "../src/pages/patientView/annotation.h"
 #include <QSurfaceFormat>
 #include <QVTKOpenGLNativeWidget.h>
+#include <vtkImageData.h>
+#include <vtkSmartPointer.h>
 
->>>>>>> main:test/tst_testeshmun.cpp
+
 // add necessary includes here
 
 class testEshmun : public QObject {
@@ -37,17 +35,9 @@ private:
   MainWindow main_window;
 };
 
-<<<<<<< HEAD:test/tst_eshmun.cpp
+bool compareFiles(const std::string &, const std::string &);
+
 testEshmun::testEshmun() { main_window.show(); }
-=======
-bool compareFiles(const std::string&, const std::string&);
-
-
-
-testEshmun::testEshmun() {
-    main_window.show();
-}
->>>>>>> main:test/tst_testeshmun.cpp
 
 testEshmun::~testEshmun() {}
 
@@ -78,31 +68,32 @@ void testEshmun::test_case1() {
 }
 
 void testEshmun::test_vtk_viewer() {
-    QSurfaceFormat::setDefaultFormat(QVTKOpenGLNativeWidget::defaultFormat());
-    Annotation* annotation = new Annotation(&main_window);
-    annotation->show();
-    SceneWidget* sceneWidget = annotation->findChild<SceneWidget*>("sceneWidget");
-    vtkSmartPointer<vtkImageData> imageData = sceneWidget->GetDummyData();
-    sceneWidget->SetImageData(imageData);
-    std::string referenceImagePath = "../test/reference_images/test_dummy.png";
-    std::string currentImagePath = "test/test_dummy.png";
-    sceneWidget->SaveScreenshot(currentImagePath);
-    QVERIFY(compareFiles(currentImagePath, referenceImagePath));
+  QSurfaceFormat::setDefaultFormat(QVTKOpenGLNativeWidget::defaultFormat());
+  Annotation *annotation = new Annotation(&main_window);
+  annotation->show();
+  SceneWidget *sceneWidget =
+      annotation->findChild<SceneWidget *>("sceneWidget");
+  vtkSmartPointer<vtkImageData> imageData = sceneWidget->GetDummyData();
+  sceneWidget->SetImageData(imageData);
+  std::string referenceImagePath = "../test/reference_images/test_dummy.png";
+  std::string currentImagePath = "test/test_dummy.png";
+  sceneWidget->SaveScreenshot(currentImagePath);
+  QVERIFY(compareFiles(currentImagePath, referenceImagePath));
 }
 
-bool compareFiles(const std::string& p1, const std::string& p2) {
-  std::ifstream f1(p1, std::ifstream::binary|std::ifstream::ate);
-  std::ifstream f2(p2, std::ifstream::binary|std::ifstream::ate);
+bool compareFiles(const std::string &p1, const std::string &p2) {
+  std::ifstream f1(p1, std::ifstream::binary | std::ifstream::ate);
+  std::ifstream f2(p2, std::ifstream::binary | std::ifstream::ate);
 
   if (f1.fail() || f2.fail()) {
-    return false; //file problem
+    return false; // file problem
   }
 
   if (f1.tellg() != f2.tellg()) {
-    return false; //size mismatch
+    return false; // size mismatch
   }
 
-  //seek back to beginning and use std::equal to compare contents
+  // seek back to beginning and use std::equal to compare contents
   f1.seekg(0, std::ifstream::beg);
   f2.seekg(0, std::ifstream::beg);
   return std::equal(std::istreambuf_iterator<char>(f1.rdbuf()),
